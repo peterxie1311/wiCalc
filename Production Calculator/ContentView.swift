6f57148e6fd1927d3ba5af82ee40c5886feba492
pdawg1311 <peterxie2000@gmail.com>
2023-12-16 21:33:06 +1000

    app working

--- Production Calculator/ContentView.swift
+++ Production Calculator/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberText = ""
+    private var comPerformance: [Double]  = [16500,17000,18000,18500,19000,19500,20000,20500,21000]
+    private var doubleValue: Double {
+            return Double(numberText) ?? 0.0
+        }
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "gpoiobe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+                    TextField("Enter numbers only (Witron Rocks!)", text: $numberText)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+            
+                Text("Entered Double Value: \(numberText)")
+                .padding()
+            
+            Text("Com Performance AVG.")
+            HStack{
+                Text("Com Performance Avg  | ")
+                Text("Hours Needed")
+            }
+            
+            
+            List {
+                ForEach(calc().calcPerformanceAVG(amount: doubleValue), id: \.self) { result in
+                               Text(result)
+                           }
+                       } .padding()
+           
+            
+            Text("Com Performance Needed")
+            HStack{
+                Text("Production Hours Remaning  | ")
+                Text("Production Needed")
+            }
+            
+            List{
+                ForEach(calc().calcHoursAVG(amount: doubleValue), id: \.self) { result in
+                               Text(result)
+                           }
+                
+            } .padding()
+            
+            
+                
+            
+            
+                }
     }
 }
 
--- Production Calculator/calc.swift
+++ Production Calculator/calc.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class calc{
+    
+    func calcPerformanceAVG(amount: Double) -> [String]{
+        var calculations: [String] = []
+        var comPerformance: [Double]  = [16500,17000,18000,18500,19000,19500,20000,20500,21000]
+        
+        for number in comPerformance {
+            
+            
+            calculations.append(String(number) + " | " + String(amount/number))
+        }
+        
+        
+        return calculations
+    }
+    
+    func calcHoursAVG(amount: Double) -> [String]{
+        var calculations: [String] = []
+        
+        for i in 2..<21 {
+            calculations.append(String(i) + " | " + String(amount/Double(i)))
+        }
+        
+        return calculations
+        
+    }
+    
+}
