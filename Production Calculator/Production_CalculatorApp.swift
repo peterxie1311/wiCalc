925f3b521387962bb8f1cfa01183ae68196ae80a
pdawg1311 <peterxie2000@gmail.com>
2023-12-16 15:31:39 +1000

    Initial Commit

---
+++ Production Calculator/Production_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Production_CalculatorApp.swift
+//  Production Calculator
+//
+//  Created by Peter Xie on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Production_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
